d10b8e601a7ac8d929d9d2cbe1f37767aaf31665
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-07-23 19:39:40 -0500

    Added react native dependencies

--- starter/App.js
+++ starter/App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+
 
 export default function App() {
   return (
