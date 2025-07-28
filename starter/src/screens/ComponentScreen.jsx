32f2fd7ec225bd57b027918b2b143a6094ab95dc
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-07-28 17:16:48 -0500

    removed bug in ComponentScreen.jsx

--- starter/src/screens/ComponentScreen.jsx
+++ starter/src/screens/ComponentScreen.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { styles } from '../styles/global';
 
 const ComponentScreen  = () => {
-    const myEmotion : string = "love";
+    const myEmotion = "love";
   return (
     <View style={styles.container}>
       <Text>This is component screen</Text>
